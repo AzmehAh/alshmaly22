47935c5e8176d1fd61d8f2cec55fb85c4b51985f
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-14 11:40:16 +0300

    Updated WhatsAppButton.tsx

--- src/components/Layout/WhatsAppButton.tsx
+++ src/components/Layout/WhatsAppButton.tsx
@@ -4,8 +4,7 @@ import { MessageCircle } from 'lucide-react';
 const WhatsAppButton = () => {
   const handleWhatsAppClick = () => {
     const phoneNumber = "963956556410"; // بدون + أو مسافات
-    const message = "Hello! Thank you for reaching out. How can we assist you ?
-";
+    const message = "Hello! Thank you for reaching out. How can we assist you ?";
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
   };
