0fa9710134f7c215fa99fc0c995ea2d4bbf778b0
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-15 10:08:54 +0300

    Updated Footer.tsx

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -51,8 +51,8 @@ const Footer = () => {
               <div className="flex items-center space-x-3">
                 <Phone size={16} className="text-[#b9a779]" />
                 <div className="text-gray-300 text-sm">
-                  <p>syria: +963 956 556 410</p>
-                  <p>turkey: +90 538 687 6411</p>
+                  <p> +963 956 556 410</p>
+                 
                 </div>
               </div>
               <div className="flex items-center space-x-3">
