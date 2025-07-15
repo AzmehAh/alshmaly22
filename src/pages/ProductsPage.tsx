bdc2bb079ee5007a73bb37ade4923011b20d12b2
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-15 16:20:36 +0300

    Updated ProductsPage.tsx

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -133,7 +133,7 @@ const ProductsPage = () => {
                   onChange={(e) => setSortBy(e.target.value)}
                   className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#b9a779] focus:border-transparent"
                 >
-                 <option value="name">{t('filters.name')}</option>
+                 <option value="name">{t('products.sort.name')}</option>
                  <option value="price-low">{t('products.sort.price_low')}</option>
                  <option value="price-high">{t('products.sort.price_high')}</option>
 
