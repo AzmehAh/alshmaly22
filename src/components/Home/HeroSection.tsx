8d3795f312672681590a43f1fcb78902fd61fdfb
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-15 13:52:19 +0300

    Updated HeroSection.tsx

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ArrowRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { useLanguage } from '../../contexts/LanguageContext';
-
+import { useTranslation } from 'react-i18next';
 const HeroSection= () => {
   const { t, i18n } = useTranslation();
   const isArabic = i18n.language === 'ar';
