e4f75cd6f32ab44908431b8c2e92ab23d207e161
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-15 11:14:53 +0300

    Updated ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -125,7 +125,6 @@ const ContactPage = () => {
                       <h3 className="font-semibold text-[#054239] mb-1">{t('contact.phone')}</h3>
                       <p className="text-gray-600">{t('contact.phone.1')}</p>
                       <p className="text-gray-600">{t('contact.phone.2')}</p>
-                   
                     </div>
                   </div>
 
