7471c314573672eb70b44022c20256c21c0e7b60
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-16 13:16:16 +0300

    Updated ProductsPage.tsx

--- src/pages/admin/ProductsPage.tsx
+++ src/pages/admin/ProductsPage.tsx
@@ -671,7 +671,7 @@ const ProductsPage = () => {
                               }}
                               className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#b9a779] focus:border-transparent"
                               dir="rtl"
-                              placeholder={t('admin.add')} {t('admin.specifications')}
+                            placeholder={`${t('admin.add')} ${t('admin.specifications')}`}
                             />
                             <button
                               type="button"
