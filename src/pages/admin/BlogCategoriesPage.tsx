82c6f165e784e32c03063af25c04f977cdb14203
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-17 10:41:51 +0300

    Updated BlogCategoriesPage.tsx

--- src/pages/admin/BlogCategoriesPage.tsx
+++ src/pages/admin/BlogCategoriesPage.tsx
@@ -265,7 +265,7 @@ const BlogCategoriesPage = () => {
                   />
                 </div>
 
-                <div className="flex justify-end space-x-3 pt-4">
+                <div className="flex justify-end  gap-3 pt-4">
                   <button
                     type="button"
                     onClick={resetForm}
