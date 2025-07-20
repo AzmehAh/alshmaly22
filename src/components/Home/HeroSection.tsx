4c15e90148824c0501de2591a17b85d00d027ea7
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-20 16:16:40 +0300

    Updated HeroSection.tsx

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -29,16 +29,9 @@ const HeroSection = () => {
   };
 
   
-  useEffect(() => {
-  const setVh = () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
-
-  setVh();
-  window.addEventListener('resize', setVh);
-
-  return () => window.removeEventListener('resize', setVh);
+ useEffect(() => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 }, []);
 
 
