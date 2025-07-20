ac70369dff5ebdeb85a93923beb5e5c8b76682a2
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-20 15:48:51 +0300

    Updated HeroSection.tsx

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -32,15 +32,25 @@ const HeroSection = () => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   };
-
   useEffect(() => {
-    setVh();
-    window.addEventListener('resize', setVh);
-    return () => window.removeEventListener('resize', setVh);
-  }, []);
+  const setVh = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+  setVh();
+  window.addEventListener('resize', setVh);
+  return () => window.removeEventListener('resize', setVh);
+}, []);
 
+
+  
   return (
-    <section id="home" className="relative  min-h-screen  flex items-center overflow-hidden w-full"
+    <section
+  id="home"
+  className="relative flex items-center overflow-hidden w-full"
+  style={{ minHeight: 'calc(var(--vh, 1vh) * 100)' }}
+>
+
     
       >
       {/* Background Video */}
