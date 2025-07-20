5fa5ee641255c70cd5d93aa7d413a4ebddeba0bf
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-20 16:14:25 +0300

    Updated HeroSection.tsx

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -50,7 +50,7 @@ const HeroSection = () => {
   className="relative flex items-center overflow-hidden w-full"
   style={{
   height: '100vh',
- style={{ height: 'calc(var(--vh, 1vh) * 100 - 4rem)' }}
+ height: 'calc(var(--vh, 1vh) * 100 - 4rem)'
  // this will override 100vh if --vh is defined
 }}
 
