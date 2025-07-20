120604867cf105cf58d459398dd55b78aae85c1e
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-20 13:07:03 +0300

    Updated HomepageManagementPage.tsx

--- src/pages/admin/HomepageManagementPage.tsx
+++ src/pages/admin/HomepageManagementPage.tsx
@@ -350,7 +350,7 @@ const HomepageManagementPage = () => {
                           <img
                             src={(item as Product).images?.[0]?.image_url || 'https://images.pexels.com/photos/1640774/pexels-photo-1640774.jpeg?auto=compress&cs=tinysrgb&w=100&h=100'}
                             alt={item.name || (item as BlogPost).title}
-                           className={`${direction === 'rtl' ? 'mr-3' : 'ml-3'}  h-10 w-10 rounded-lg object-cover`}
+                           className={`${direction === 'rtl' ? 'ml-3' : 'mr-3'}  h-10 w-10 rounded-lg object-cover`}
                           />
                         )}
                         <div>
