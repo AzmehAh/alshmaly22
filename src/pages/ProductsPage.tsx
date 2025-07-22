34039983d3931f3fb9a85b2db1f1d30cee644ce3
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-22 16:48:27 +0300

    Updated ProductsPage.tsx

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -71,8 +71,8 @@ const ProductsPage = () => {
 
         <div className="flex flex-col lg:flex-row gap-8">
           {/* Filters */} 
-          <div className="lg:w-1/4 w-full self-start">
-            <div className="bg-[#f7f7f7] rounded-2xl p-6 shadow-lg sticky top-24 z-10">
+          <div className="lg:w-1/4 w-full ">
+            <div className="bg-[#f7f7f7] rounded-2xl p-6 shadow-lg  z-10">
               <h3 className="text-xl font-semibold text-[#054239] mb-6 flex items-center">
                 <Filter size={20} className="mr-2" />
                 {t('products.filters.title')}
