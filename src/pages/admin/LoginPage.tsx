b61befcbde8c100ca380ce6850e63c228d445ec6
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-24 14:53:20 +0300

    Updated LoginPage.tsx

--- src/pages/admin/LoginPage.tsx
+++ src/pages/admin/LoginPage.tsx
@@ -38,7 +38,7 @@ const LoginPage = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8" dir={direction}>
+      <div className="max-w-md w-full space-y-8" >
         <div className="text-center">
           <img
             src="https://i.postimg.cc/0Q8pxwTM/logo.png"
