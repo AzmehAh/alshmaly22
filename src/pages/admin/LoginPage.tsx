68588c608a884b80b9157c73f74357ebc10a1c08
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-07-24 14:50:36 +0300

    Updated LoginPage.tsx

--- src/pages/admin/LoginPage.tsx
+++ src/pages/admin/LoginPage.tsx
@@ -126,10 +126,7 @@ const LoginPage = () => {
           </button>
         </form>
 
-        {/* مثال زر بترجمة admin.add */}
-        <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded">
-          {t('admin.add')}
-        </button>
+        
       </div>
     </div>
   );
