4c354b8e868673328be9bbdf9e6b8063ce8bf996
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-08-09 17:13:49 +0300

    Remove bucket initialization from client

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { initializeStorageBuckets } from './utils/supabaseStorage';
-
-// Initialize storage buckets on app start
-initializeStorageBuckets();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
