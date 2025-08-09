a556f9392582db6b33df21f9f8a22cea3f34369e
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-08-09 15:25:08 +0300

    Updated ProductsPage.tsx

--- src/pages/admin/ProductsPage.tsx
+++ src/pages/admin/ProductsPage.tsx
@@ -779,7 +779,10 @@ const ProductsPage = () => {
                   {/* شبكة الصور */}
                   {images.length > 0 && (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                      {images.map((image, index) => (
+                    {[...images]
+  .sort((a, b) => (a.sort_order || 0) - (b.sort_order || 0))
+  .map((image, index) => (
+
                         <div
                           key={index}
                           className={`relative bg-white border-2 rounded-xl overflow-hidden transition-all duration-300 ${
