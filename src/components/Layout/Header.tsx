6a84481004c275159d7415eba3c303826abc8f3d
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-14 12:18:56 +0300

    Updated Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -106,7 +106,7 @@ const Header = () => {
   <img
     src="./images/cert2.jpeg"
     alt="Certificate 2"
-    className="h-13 w-13 object-contain"
+    className="h-12.5 w-12.5 object-contain"
   />
 </div>
           </nav>
